2fd008220bea5395ebbbe6787a58814b3c9da3a1
robert <757120778@qq.com>
2023-09-19 19:15:43 +0800

    first

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**************************************************************
+ * File Name   : index.php
+ * Author      : robert
+ * Create Time : 2023-09-19 18:56
+ * DESC        :
+ *************************************************************/
+
+require_once __DIR__.'/vendor/autoload.php';
+
+$test = new \Package\Test();
+$test->index();
+
+test();
